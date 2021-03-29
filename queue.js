9788e5e00fc77266c5ff4e99787c04788edcdff4
Alex Betita <alexbheb25@gmail.com>
2021-03-28 23:40:05 -0700

    queue

--- queue.js
+++ queue.js
@@ -1,14 +1,55 @@
-class Node{
+class Node {
   constructor(value){
-    this.value = value;
     this.next = null;
+    this.value = value;
   }
 }
 
-class Queue{
+class Queue {
   constructor(){
-    this.head = null;
-    this.tail = null;
+    this.front = null;
+    this.back = null;
     this.length = 0;
   }
+
+  enqueue(value){
+    if(!this.front){
+      const newNode = new Node(value);
+      this.front = newNode;
+      this.back = newNode;
+      this.length++
+    } else {
+      const newNode = new Node(value);
+      const prevBack = this.back;
+      this.back = newNode;
+      prevBack.next = newNode;
+      this.length++;
+    }
+
+    return this.size();
+  }
+
+
+  dequeue(){
+    if(!this.front){
+      return null;
+    } else if(this.size() === 1){
+      const removeFront = this.front;
+      this.front = null;
+      this.back = null;
+      this.length--;
+      return removeFront.value;
+    } else {
+      const newFront = this.front.next;
+      const removeFront = this.front;
+      this.front = newFront;
+      this.length--;
+      return removeFront.value;
+    }
+  }
+
+  size(){
+    return this.length
+  }
+
 }

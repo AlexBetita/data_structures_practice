9a9cced7381c4ae22e61f9cf51584595134e9df4
Alex Betita <alexbheb25@gmail.com>
2021-03-28 23:40:29 -0700

    stack

--- stack.js
+++ stack.js
@@ -1,42 +1,53 @@
-class Node{
+class Node {
   constructor(value){
-    this.value = value;
-    this.next = null;
+      this.value = value;
+      this.next = null;
   }
+
 }
 
-class Stack{
+class Stack {
   constructor(){
-    this.top = null;
-    this.length = 0;
+      this.top = null;
+      this.head = null;
+      this.length = 0;
   }
 
   push(value){
-    if(!this.top){
-      const newNode = new Node(value)
-      this.top = newNode;
-      this.length++;
-    } else {
-      const newNode = newNode(value)
-      const prevTop = this.top;
-      this.top = newNode;
-      prevTop.next = newNode;
-      this.length++;
-    }
+      if(!this.head){
+          const newNode = new Node(value);
+          this.head = newNode;
+          this.top = newNode;
+          this.length++;
+      } else {
+          const newNode = new Node(value);
+          const prevTop = this.top;
+          this.top = newNode;
+          newNode.next = prevTop;
+          this.length++;
+      }
+      return this.size()
   }
 
   pop(){
-    if(!this.top){
-      return -1
-    }
-    else if(this.size() === 1){
-
-    } else {
-      
-    }
+      if(!this.head){
+          return null
+      } else if(this.size() === 1){
+          const removeHead = this.head;
+          this.head = null;
+          this.top = null;
+          this.length--;
+          return removeHead.value;
+      } else {
+          const removeTop = this.top;
+          const value = removeTop.value
+          this.top = removeTop.next;
+          this.length--;
+          return value
+      }
   }
 
   size(){
-    return this.length
+      return this.length
   }
 }
